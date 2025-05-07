978d4314d978f4a4ab581904f86992d9416ade39
jsphu <yunal22@ogr.iu.edu.tr>
2025-05-07 15:29:48 +0300

    added base code of SMILES to Image

---
+++ str2smi/CLI.py
@@ -0,0 +1,23 @@
+from rdkit import Chem
+from rdkit.Chem import Draw, AllChem
+from rdkit.Chem.Draw import rdMolDraw2D
+from rdkit.Chem.Draw.rdMolDraw2D import MolDraw2DCairo
+from sys import argv
+from shutil import get_terminal_size
+
+smiles = argv[1]
+mol = Chem.MolFromSmiles(smiles)
+AllChem.Compute2DCoords(mol)
+
+width, height = get_terminal_size()
+drawer = MolDraw2DCairo(width, height)
+opts = drawer.drawOptions()
+
+opts.backgroundColour = (0, 0, 0)
+opts.useBWAtomPalette()
+
+drawer.DrawMolecule(mol)
+drawer.FinishDrawing()
+
+with open("tmp-mol.png","wb") as f:
+    f.write(drawer.GetDrawingText())
